37aaf27829fea5204e462d036c89e5bec388d935
Marlen <ma000161@gmail.com>
2024-05-01 15:41:46 -0400

    * models.py: Removed an empty line * services.py: 	+ Updated crear\_registro\_contable to get the Vehiculo object using its patente 	+ Modified deshabilitar\_cho

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class Vehiculo (models.Model):
     marca = models.CharField(max_length=20, null=False,blank=False)
     modelo = models.CharField(max_length=20, null=False,blank=False)
     year = models.IntegerField(null=False,blank=False)
+    activo = models.BooleanField(default=False)
 
 
 class Chofer (models.Model):
--- app/services.py
+++ app/services.py
@@ -2,8 +2,8 @@
 from app.models import Chofer, Vehiculo, RegistrarContabilidad
 
 
-def crear_vehiculo(patente, marca, modelo, year): 
-    vehiculo = Vehiculo(patente=patente, marca=marca, modelo=modelo, year=year)
+def crear_vehiculo(patente, marca, modelo, year, activo): 
+    vehiculo = Vehiculo(patente=patente, marca=marca, modelo=modelo, year=year, activo=activo)
     vehiculo.save() 
     return vehiculo
 
@@ -12,4 +12,52 @@ def crear_chofer(rut, nombre, apellido, activo):
     chofer.save()
     return chofer
 
+def crear_registro_contable(fecha_compra, valor, vehiculo):
+    registro_contable = RegistrarContabilidad(fecha_compra=fecha_compra, valor=valor, vehiculo=vehiculo)
+    registro_contable.save()
+    return registro_contable
 
+def deshabilitar_chofer(chofer):
+    chofer.activo = False
+    chofer.save()
+    return chofer
+
+def deshabilitar_vehiculo(vehiculo):
+    vehiculo.activo = False
+    vehiculo.save()
+    return vehiculo 
+
+def  habilitar_chofer(chofer):
+    chofer.activo = True
+    chofer.save()
+    return chofer  
+
+def habilitar_vehiculo(vehiculo):
+    vehiculo.activo = True
+    vehiculo.save()
+    return vehiculo
+
+def obtener_vehiculo(patente):
+    vehiculos = Vehiculo.objects.all(patente=patente)
+    return vehiculos
+
+def obtener_chofer(rut):
+    chofer = Chofer.objects.get(rut=rut)
+    return chofer
+
+def  asignar_chofer_a_vehiculo(vehiculo,chofer):
+    vehiculo.chofer = chofer
+    vehiculo.save()
+    chofer.save()
+
+def imprimir_datos_vehiculos(): 
+    vehiculos = Vehiculo.objects.all() 
+    for v in vehiculos: 
+        print(f"Vehiculo:{v.patente}/{v.marca}/{v.modelo}/" + 
+              f"{v.year}/activo:{v.activo}") 
+        if hasattr(v, "chofer"): 
+            print(f"\tChofer[{v.chofer.rut}]:{v.chofer.nombre} " + 
+                  f"{v.chofer.apellido}/activo:{v.chofer.activo}") 
+        if hasattr(v, "contabilidad"): 
+            print(f"\tContabilidad:[{v.contabilidad.id}]:fecha_compra:" + 
+                  f"{v.contabilidad.fecha_compra}/valor:{v.contabilidad.valor}")
